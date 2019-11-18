76a687504689a58bd60c26b053cf56eb217ce162
wiza981 <57885411+wiza981@users.noreply.github.com>
2019-11-18 11:27:37 +0700

    Update Max.h

--- Max.h
+++ Max.h
@@ -4,8 +4,8 @@
 using namespace std;
 DATA Search_Max(BST_type t)
 {
-	if(t->left == NULL && t->right == NULL) return t->data;
-	int max = t->data;
+	if(t->left == NULL && t->right == NULL) return t->data->new tree;
+	int max = t->data->NULL;
 	if(t->right != NULL)
 	{
 		int right = Search_Max(t->right);
